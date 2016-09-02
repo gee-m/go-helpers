30197fca374cd3be30f3c3144b263c1a2cc4be14
Guillaume Merindol <guillaume@processout.com>
2016-09-02 14:48:21 -0400

    gslide package

---
+++ gslice/helpers.go
@@ -0,0 +1,13 @@
+package gslice
+
+/* Add this to user snippets for go
+   "Remove element of index i from slive": {
+       "prefix": "gslice.Remove",
+       "body": [
+           "${1:array} = append(${1:array}[:${2:i}], ${1:array}[${2:i}+1:]...)"
+       ]
+   },
+*/
+
+// TODO CREATE A BOOLEAN LOGIC "RULES" PACKAGE, WHERE EASY DEFINE FUNCTION FOR
+// MAKING LOGIC MORE READABLE!!!
