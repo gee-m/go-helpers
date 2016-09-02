299eaa7793c132a7ea7c3d80922554bd7035af11
Guillaume Merindol <guillaume@processout.com>
2016-09-02 14:33:36 -0400

    MatchingProblem()

--- gstring/helpers.go
+++ gstring/helpers.go
@@ -6,3 +6,9 @@ func SetIfExists(dst *string, src string) {
 		*dst = src
 	}
 }
+
+// MatchingProblem returns a formatted string which explains
+// that the two elements s1 and s2 of type obj don't match
+func MatchingProblem(obj, s1, s2 string) string {
+	return obj + " " + s1 + " and " + s2 + " don't match"
+}
