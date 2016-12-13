6edbe564e65e905b2007687c9861ea22b9d4e0df
Guillaume Merindol <guillaume@merindol.co>
2016-12-13 15:09:52 -0500

    Gdebug: update and debug count print

--- gdebug/print.go
+++ gdebug/print.go
@@ -7,7 +7,14 @@ var count = 0
 // CountPrintf prints the debug message along with a prefix number to identify it
 func CountPrintf(f string, vals ...interface{}) {
 	count++
-	fmt.Println("%d - "+f, count, vals)
+	fmt.Printf(fmt.Sprintf("%d - ", count)+f, vals...)
+}
+
+// CountPrintln prints the debug message along with a prefix number to identify it
+func CountPrintln(f ...interface{}) {
+	count++
+	fmt.Printf("%d - ", count)
+	fmt.Println(f...)
 }
 
 // ResetCount resets the count used in CountPrinf
