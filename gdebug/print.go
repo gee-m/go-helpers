056b06ceb0ffcdc2d9387c53d8c864ccaa5c8cc4
Guillaume Merindol <guillaume@merindol.co>
2017-03-02 11:46:58 +0100

    gdebug: add Debugf print

--- gdebug/print.go
+++ gdebug/print.go
@@ -1,8 +1,20 @@
 package gdebug
 
-import "fmt"
+import (
+	"fmt"
+	"runtime/debug"
+	"strings"
+)
 
 var count = 0
+
+// Debugf prints the filename:line_nb of the Debugf call
+func Debugf(f string, vals ...interface{}) {
+	stack := strings.Split(string(debug.Stack()), "\n")
+	line := stack[6]
+	line = line[strings.LastIndex(line, "/")+1:]
+	fmt.Printf(fmt.Sprintf("%s: ", line)+f, vals...)
+}
 
 // CountPrintf prints the debug message along with a prefix number to identify it
 func CountPrintf(f string, vals ...interface{}) {
