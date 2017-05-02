0a6d4f7d5b7cc04d597904970dee70ca2597a492
Guillaume Merindol <guillaume@merindol.co>
2017-05-02 10:45:13 +0200

    Add Panicf

--- gmisc/helpers.go
+++ gmisc/helpers.go
@@ -1,7 +1,15 @@
 package gmisc
 
+import "fmt"
+
+// PanicIf panics if the interface passed is not nil
 func PanicIf(i interface{}) {
 	if i != nil {
 		panic(i)
 	}
+}
+
+// Panicf panics with the format string and its parameters
+func Panicf(format string, a ...interface{}) {
+	panic(fmt.Sprintf(format, a))
 }
