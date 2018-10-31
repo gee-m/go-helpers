4d471f0c1412246cdfda89cce100065ab1397238
Guillaume Merindol <guillaume@merindol.co>
2018-10-31 16:38:44 +0100

    gdebug: add two stack printing functions

--- gdebug/print.go
+++ gdebug/print.go
@@ -3,6 +3,7 @@ package gdebug
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"runtime/debug"
 	"strings"
 
@@ -31,6 +32,55 @@ func lineInfo(n int) string {
 	line = line[strings.LastIndex(line, "/")+1:]
 	line = strings.Split(line, " ")[0]
 	return line
+}
+
+// StackIgnoreFirst retuns the stack with the first n stack items ignored
+// (starting at called position)
+func StackIgnoreFirst(n int) string {
+	stack := strings.Split(string(debug.Stack()), "\n")
+	// first line is goroutine, we always keep dat
+	// then lines come in two: top line is function, bottom is file:line info
+
+	// since we start from the second line (first is goroutine info) and since
+	// we start n from the caller, we skip 2 additional (debug func + here).
+	// We multiply by 2 to get rid of the pairs described above.
+	lines := append(stack[0:1], stack[(n+3)*2:]...)
+	return strings.Join(lines, "\n")
+}
+
+// SmartStack returns the stack starting with exactly the line that panick'ed
+// (goroutine is mentioned still as first line)
+func SmartStack() string {
+	gopath := os.Getenv("GOPATH")
+	stack := strings.Split(string(debug.Stack()), "\n")
+
+	// we look for panic, we start at that
+	count := 0
+	for _, s := range stack {
+		if strings.Contains(s, "runtime/panic.go:") {
+			break
+		}
+		count++
+	}
+
+	// first line is goroutine nb, we keep dat
+	lines := append(stack[0:1], stack[count+1:]...)
+
+	var total string
+	for i := range lines {
+		if i == 0 {
+			total += lines[i]
+			continue // goroutine line
+		}
+
+		if i%2 == 1 {
+			total += "\n  "
+		} else {
+			total += "\n"
+		}
+		total += strings.Replace(lines[i], gopath, "$GOPATH", 1)
+	}
+	return total
 }
 
 // Debugf prints the filename:line_nb of the Debugf call
